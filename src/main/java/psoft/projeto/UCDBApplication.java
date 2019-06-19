a7fc57d651d32810b54e9196da870d00a920638b
Igor Seabra <igor.seabra@ccc.ufcg.edu.br>
2019-06-19 16:47:01 -0300

    update maven

--- src/main/java/psoft/projeto/UCDBApplication.java
+++ src/main/java/psoft/projeto/UCDBApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import psoft.projeto.controller.CourseController;
 import psoft.projeto.controller.TokenFilter;
@@ -19,10 +22,19 @@ public class UCDBApplication {
 	
 	@Bean
 	public FilterRegistrationBean<TokenFilter> filterJwt() {
-		FilterRegistrationBean<TokenFilter> filter = new FilterRegistrationBean<TokenFilter>();
-		filter.setFilter(new TokenFilter());
+		FilterRegistrationBean<TokenFilter> filter = new FilterRegistrationBean<TokenFilter>(new TokenFilter());
 		filter.addUrlPatterns(CourseController.getPrivatePatterns());
 		return filter;
 	}
-
+	
+	@Bean
+	public FilterRegistrationBean corsFilter2() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
 }
