af82cc1b1c2daf1dc575a986914b30e8ccd2fdea
Igor Seabra <igor.seabra@ccc.ufcg.edu.br>
2019-06-19 17:11:19 -0300

    .

--- src/main/java/psoft/projeto/model/User.java
+++ src/main/java/psoft/projeto/model/User.java
@@ -4,8 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Entity
+@Table(name = "TAB_USUARIO")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
